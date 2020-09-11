47b44c6c62059d0483cccc0d43885c85b9feb160
Narendra Singh <narensingh213@gmail.com>
2020-09-11 16:26:04 +0530

    Fix UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2, ElementRef, ViewChildren, QueryList } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, ViewChildren, QueryList, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +8,7 @@ import { Component, OnInit, Renderer2, ElementRef, ViewChildren, QueryList } fro
 export class HomeComponent implements OnInit {
 
   @ViewChildren('drop') dropEl: QueryList<ElementRef>;
+  @ViewChild('district') district: ElementRef;
 
   constructor(
     private _renderer: Renderer2,
@@ -18,7 +19,7 @@ export class HomeComponent implements OnInit {
   
   ngAfterViewInit(): void {
     this.dropEl.map(el => {
-      this._renderer.listen(el.nativeElement, 'click', (event) => {
+      this._renderer.listen(el.nativeElement, 'click', (event: any) => {
         if (el.nativeElement.classList.contains('open')) {
           this._renderer.removeClass(el.nativeElement, 'open');
         } else {
@@ -26,6 +27,28 @@ export class HomeComponent implements OnInit {
         }
       });
     })
+
+    this._renderer.listen(this.district.nativeElement, 'click', (event: any) => {
+      let parent: Element = this.district.nativeElement.parentElement;
+      if (parent.classList.contains('open')) {
+        this._renderer.removeClass(parent, 'open');
+      } else {
+        this._renderer.addClass(parent, 'open');
+      }
+    })
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: any): any {
+    this.dropEl.forEach(el => {
+      if (event.target.parentElement != el.nativeElement) {
+        this._renderer.removeClass(el.nativeElement, 'open');
+      }
+    });
+
+    if (event.target != this.district.nativeElement) {
+      this._renderer.removeClass(this.district.nativeElement.parentElement, 'open');
+    }
   }
 
 }
